316d0c1159a5427426682f051925cf0b7cb8f233
Ryad ZENINE <r.zenine@gmail.com>
2020-10-24 22:37:23 +0200

    feat(core::vars): first modelisation of vars

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,2 +1,3 @@
 pub mod aliases;
 pub mod scripts;
+pub mod vars;
---
+++ src/core/vars.rs
@@ -0,0 +1,38 @@
+use serde::{Deserialize, Serialize};
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct Var {
+    name: String,
+    desc: String,
+    choices: Vec<Choice>,
+}
+
+impl Var {
+    pub fn new<IntoStr>(name: IntoStr, desc: IntoStr, choices: Vec<Choice>) -> Var
+    where
+        IntoStr: Into<String>,
+    {
+        Var {
+            name: name.into(),
+            desc: desc.into(),
+            choices: choices,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct Choice {
+    value: String,
+    desc: String,
+}
+
+impl Choice {
+    pub fn new<IntoStr>(value: IntoStr, desc: IntoStr) -> Choice
+    where
+        IntoStr: Into<String>,
+    {
+        Choice {
+            value: value.into(),
+            desc: desc.into(),
+        }
+    }
+}
