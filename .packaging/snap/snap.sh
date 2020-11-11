cfd3113a3b0bb27497781bd5e0da84d67ad86b73
Ryad ZENINE <r.zenine@gmail.com>
2020-11-12 00:13:22 +0100

    ci(snapcraft): fixiging snapcraft.yaml

--- .packaging/snap/snap.sh
+++ .packaging/snap/snap.sh
@@ -5,4 +5,4 @@ cd .packaging/snap
 mkdir .snapcraft
 echo $SNAP_LOGIN_FILE | base64 --decode --ignore-garbage > .snapcraft/snapcraft.cfg
 snapcraft
-snapcraft upload *.snap --release stable
+snapcraft push *.snap --release stable
