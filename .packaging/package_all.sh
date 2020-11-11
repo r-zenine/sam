abe399664dd643bc52bcf4130f36e6653b1d5957
Ryad ZENINE <r.zenine@gmail.com>
2020-11-11 23:07:14 +0100

    ci(packaging): add's snap support

---
+++ .packaging/package_all.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# change directory to top root of git project.
+cd `git rev-parse --show-toplevel`
+export PROJECT="sam"
+export PROJECT_URL="https://github.com/r-zenine/ssam"
+export PROJECT_LICENCE="GPL-3.0"
+export PROJECT_DESCRIPTION="sam lets you difine custom aliases and search them using fuzzy search."
+export APP_VERSION=$(grep Cargo.toml -e "^version = " |sed 's/version \=//' |sed 's/\"//g'|sed 's/ //g')
+export ARCHIVE_PATH="./target/x86_64-apple-darwin/release/${PROJECT}_macos_x86_64_v${APP_VERSION}.tar.gz"
+export RELEASE_HASH=$(sha256sum ${ARCHIVE_PATH}|cut -d\  -f1)
+
+envsubst < .packaging/homebrew/ssam.rb.j2 > .packaging/homebrew/ssam.rb
+envsubst < .packaging/snap/snapcraft.yaml.j2 > .packaging/snap/snapcraft.yaml
+
+./.packaging/homebrew/homebrew.sh
+./.packaging/snap/snap.sh
---
+++ .packaging/snap/snap.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd `git rev-parse --show-toplevel`
+cd .packaging/snap
+
+mkdir .snapcraft
+echo $SNAP_LOGIN_FILE | base64 --decode --ignore-garbage > .snapcraft/snapcraft.cfg
+snapcraft
+snapcraft push *.snap --release stable
