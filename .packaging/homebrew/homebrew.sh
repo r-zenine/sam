06381e4923e11337f12936e335b64b9d73532ce9
Ryad ZENINE <r.zenine@gmail.com>
2020-11-12 15:20:05 +0100

    ci(packaging): first attempt at homebrew

--- .packaging/homebrew/homebrew.sh
+++ .packaging/homebrew/homebrew.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+git submodule --init
+git submodule --update --recursive
+cd `git rev-parse --show-toplevel`
+cd .packaging/homebrew
+cp ssam.rb homebrew-ssam/Formula/ssam.rb
+cd homebrew-ssam
+git add --all
+git commit -a -m"bump version"
