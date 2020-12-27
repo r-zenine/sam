47537b435e49449dd9cbc08672632fd49d71a4b0
Ryad Zenine <r.zenine@gmail.com>
2020-12-27 22:04:32 +0100

    feat(utils::processes): handle environment variables

--- src/utils/processes.rs
+++ src/utils/processes.rs
@@ -39,6 +39,7 @@ where
     fn into(self) -> Command {
         let mut command = Command::new(current_shell_or_sh());
         command.arg("-c").arg(self.command);
+        command.envs(env::vars());
         let curr_dir = std::env::current_dir();
         if let Ok(dir) = curr_dir {
             command.current_dir(dir);
