9972e16aa9f5628f3be11a1c5d71e9409f109486
Ryad ZENINE <r.zenine@gmail.com>
2021-01-07 21:22:04 +0100

    fix(bin::sam): read's local config file first

--- src/bin/sam/config.rs
+++ src/bin/sam/config.rs
@@ -3,8 +3,9 @@ use sam::utils::fsutils::ErrorsFS;
 use serde::{Deserialize, Serialize};
 use std::path::{Path, PathBuf};
 use thiserror::Error;
-// Todo
-// 1. get rid of RawAppSettings.
+
+const CONFIG_FILE_NAME: &str = ".sam_rc.toml";
+
 #[derive(Debug, Serialize, Deserialize, Default, Clone)]
 pub struct AppSettings {
     root_dir: PathBuf,
@@ -13,7 +14,7 @@ pub struct AppSettings {
 type Result<T> = std::result::Result<T, ErrorsConfig>;
 
 impl AppSettings {
-    fn load_from_path(path: PathBuf) -> Result<config::Config> {
+    fn read_config(path: PathBuf) -> Result<config::Config> {
         let path = fsutils::ensure_exists(path)
             .and_then(fsutils::ensure_is_file)
             .and_then(fsutils::ensure_sufficient_permisions)?;
@@ -24,17 +25,14 @@ impl AppSettings {
     }
 
     pub fn load() -> Result<Self> {
-        let home_dir_o = dirs::home_dir()
-            .map(|e| e.join(".sam_rc.toml"))
-            .ok_or(ErrorsConfig::CantFindHomeDirectory)?;
-        let current_dir_o = std::env::current_dir()
-            .map_err(|_| ErrorsConfig::CantFindCurrentDirectory)
-            .map(|e| e.join("sam_rc.toml"))?;
+        let home_dir_o = Self::home_dir_config_path()?;
+        let current_dir_o = Self::current_dir_config_path()?;
 
-        let config_home_dir = Self::load_from_path(home_dir_o);
-        let config_current_dir = Self::load_from_path(current_dir_o);
-        config_home_dir
-            .or(config_current_dir)?
+        let config_home_dir = Self::read_config(home_dir_o);
+        let config_current_dir = Self::read_config(current_dir_o);
+
+        config_current_dir
+            .or(config_home_dir)?
             .try_into::<Self>()
             .map_err(ErrorsConfig::from)
             .and_then(AppSettings::validate)
@@ -50,6 +48,18 @@ impl AppSettings {
             fsutils::ensure_exists(f).and_then(fsutils::ensure_sufficient_permisions)?;
         }
         Ok(orig)
+    }
+
+    fn home_dir_config_path() -> Result<PathBuf> {
+        dirs::home_dir()
+            .map(|e| e.join(CONFIG_FILE_NAME))
+            .ok_or(ErrorsConfig::CantFindHomeDirectory)
+    }
+
+    fn current_dir_config_path() -> Result<PathBuf> {
+        std::env::current_dir()
+            .map_err(|_| ErrorsConfig::CantFindCurrentDirectory)
+            .map(|e| e.join(CONFIG_FILE_NAME))
     }
 }
 
