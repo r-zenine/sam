31f752b3311f85236437cc0a29f07060e97e4298
Ryad ZENINE <r.zenine@gmail.com>
2021-10-18 11:42:24 +0100

    refactor(sam-utils): extract utility functions in their own trait

--- src/bin/sam/userinterface.rs
+++ src/bin/sam/userinterface.rs
@@ -1,11 +1,11 @@
 use prettytable::{cell, format, row, Table};
 use sam::io::readers::read_choices;
-use sam::utils::fsutils::{ErrorsFS, TempFile};
 use sam_core::aliases::Alias;
 use sam_core::choices::Choice;
 use sam_core::dependencies::{Dependencies, ErrorsResolver, Resolver};
 use sam_core::identifiers::Identifier;
 use sam_core::processes::ShellCommand;
+use sam_utils::fsutils::{ErrorsFS, TempFile};
 use skim::prelude::*;
 use std::cell::RefCell;
 use std::collections::HashMap;
