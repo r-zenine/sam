fe685126bf3d8e421a753e64ae46ae8feb19d718
Ryad ZENINE <r.zenine@gmail.com>
2022-03-03 22:44:19 +0100

    refactor(sam-persistence): fix linter warnings in crate

--- sam-persistence/src/sequential_state.rs
+++ sam-persistence/src/sequential_state.rs
@@ -34,7 +34,7 @@ pub enum ErrorSequentialState {
 
 pub type ModResult<V> = std::result::Result<V, ErrorSequentialState>;
 
-type FDB<V> = FileDatabase<Vec<V>, Ron>;
+type Fdb<V> = FileDatabase<Vec<V>, Ron>;
 
 pub trait Value: Serialize + DeserializeOwned + Send + Clone + std::fmt::Debug {}
 impl<T> Value for T where T: Serialize + DeserializeOwned + Send + Clone + std::fmt::Debug {}
@@ -67,12 +67,14 @@ where
         db.save().map_err(ErrorSequentialState::SaveFailures)
     }
 
+    #[allow(dead_code)]
     pub fn last(&self) -> ModResult<Option<V>> {
         let db = self.open_db()?;
         db.read(|db| db.last().map(Clone::clone))
             .map_err(ErrorSequentialState::ReadFailure)
     }
 
+    #[allow(dead_code)]
     pub fn first(&self) -> ModResult<Option<V>> {
         let db = self.open_db()?;
         db.read(|db| db.first().map(Clone::clone))
@@ -85,6 +87,7 @@ where
             .map_err(ErrorSequentialState::ReadFailure)
     }
 
+    #[allow(dead_code)]
     pub fn delete(&self, position: usize) -> ModResult<()> {
         let db = self.open_db()?;
         db.write(|db| {
@@ -94,69 +97,10 @@ where
         db.save().map_err(ErrorSequentialState::SaveFailures)
     }
 
-    fn open_db(&self) -> ModResult<FDB<V>> {
-        Ok(FDB::<V>::load_from_path(&self.path)
-            .or(FDB::<V>::create_at_path(&self.path, vec![]))
-            .map_err(ErrorSequentialState::OpenFailure)?)
-    }
-}
-
-pub trait EntrySelector<V> {
-    fn select_entry(
-        &self,
-        data: impl Iterator<Item = (usize, V)>,
-    ) -> Result<Option<usize>, Box<dyn std::error::Error>>;
-}
-
-#[derive(Debug, Error)]
-pub enum ErrorSeqStateInteractor {
-    #[error("failed to interract with state because \n->{0}")]
-    ErrState(#[from] ErrorSequentialState),
-    #[error("failed to select entry because\n->{0}")]
-    ErrEntrySelector(Box<dyn std::error::Error>),
-}
-
-pub struct SequentialStateInteractor<V, D> {
-    state: SequentialState<V>,
-    delegate: D,
-}
-
-impl<V, D> SequentialStateInteractor<V, D> {
-    fn new(path: impl AsRef<Path>, delegate: D, max_size: Option<usize>) -> Self {
-        SequentialStateInteractor {
-            state: SequentialState {
-                path: path.as_ref().to_path_buf(),
-                _marker: PhantomData::default(),
-                max_size,
-            },
-            delegate,
-        }
-    }
-}
-
-impl<V, D> SequentialStateInteractor<V, D>
-where
-    D: EntrySelector<V>,
-    V: Value,
-{
-    fn delete_entry(&self) -> Result<(), ErrorSeqStateInteractor> {
-        let state_iterator = self.entries()?;
-        let selected_element = self
-            .delegate
-            .select_entry(state_iterator)
-            .map_err(ErrorSeqStateInteractor::ErrEntrySelector)?;
-        if let Some(position) = selected_element {
-            self.state.delete(position)?;
-        }
-        Ok(())
-    }
-}
-impl<V, D> SequentialStateInteractor<V, D>
-where
-    V: Value,
-{
-    fn entries(&self) -> Result<impl Iterator<Item = (usize, V)>, ErrorSeqStateInteractor> {
-        Ok(self.state.entries()?.enumerate())
+    fn open_db(&self) -> ModResult<Fdb<V>> {
+        Fdb::<V>::load_from_path(&self.path)
+            .or_else(|_| Fdb::<V>::create_at_path(&self.path, vec![]))
+            .map_err(ErrorSequentialState::OpenFailure)
     }
 }
 
@@ -204,10 +148,4 @@ mod tests {
         assert_eq!(state.first().expect("could not get first element"), Some(1));
         assert_eq!(state.last().expect("could not get last element"), Some(7));
     }
-
-    #[test]
-    fn test_sequential_state_interactor_entries() {}
-
-    #[test]
-    fn test_sequential_state_interactor_delete_entry() {}
 }
