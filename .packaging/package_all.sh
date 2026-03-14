cd573d114f6b369bf74248a078097f69b2a4b8ef
Ryad ZENINE <r.zenine@gmail.com>
2026-03-14 19:45:31 +0100

    fix(packaging): read version from sam-cli/Cargo.toml instead of workspace root

--- .packaging/package_all.sh
+++ .packaging/package_all.sh
@@ -5,7 +5,7 @@ export PROJECT="sam"
 export PROJECT_URL="https://github.com/r-zenine/sam"
 export PROJECT_LICENCE="GPL-3.0"
 export PROJECT_DESCRIPTION="sam lets you define custom aliases and search them using fuzzy search."
-export APP_VERSION=$(grep Cargo.toml -e "^version = " |sed 's/version \=//' |sed 's/\"//g'|sed 's/ //g')
+export APP_VERSION=$(grep sam-cli/Cargo.toml -e "^version = " |sed 's/version \=//' |sed 's/\"//g'|sed 's/ //g')
 export ARCHIVE_X86_64="./target/release/sam_macos_x86_64_v${APP_VERSION}.tar.gz"
 export ARCHIVE_AARCH64="./target/release/sam_macos_aarch64_v${APP_VERSION}.tar.gz"
 export RELEASE_HASH_X86_64=$(sha256sum ${ARCHIVE_X86_64} | cut -d\  -f1)
